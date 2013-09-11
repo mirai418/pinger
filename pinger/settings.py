6631b1baf83051ed8746156a2c25d536d1e610cf
Mirai Akagawa <mirai418@me.com>
2013-09-11 14:08:43 -0400

    pushing to heroku

--- pinger/settings.py
+++ pinger/settings.py
@@ -154,3 +154,13 @@ LOGGING = {
         },
     }
 }
+
+######################### HEROKU DB STUFF ##########################
+
+# if os.environ.get('MYSITE_PRODUCTION', False):
+#Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
